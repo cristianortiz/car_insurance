3f2a0ccc11317f073d7fb1a6203ce3394f339b6f
cristian <cristian.ortiz@uda.cl>
2021-03-21 00:42:40 -0300

    more styles for form component, useStates for handle data inputs and error

--- src/components/InsuranceForm.js
+++ src/components/InsuranceForm.js
@@ -1,35 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
+import { getYearDifference } from "../helper";
 
+//styled div to form fields
+const FormDiv = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+  -webkit-appearance: none; //replace the default browser select style
+`;
+
+const InputRadio = styled.input`
+  margin: 0 1rem;
+`;
+
+const Button = styled.button`
+  background-color: #00838f;
+  margin-top: 2rem;
+  font-size: 16px;
+  width: 100%;
+  padding: 1rem;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  border: none;
+  transition: background-color 0.3s ease;
+
+  //Sass for hover
+  &:hover {
+    cursor: pointer;
+    background-color: #26c6da;
+  }
+`;
+
+const Error = styled.div`
+  background-color: red;
+  color: white;
+  padding: 1rem 0 1rem 0;
+  width: 100%;
+  text-align: center;
+  margin: 0 0 2rem 0;
+`;
 const InsuranceForm = () => {
-  //styled div to form fields
-  const FormDiv = styled.div`
-    display: flex;
-    margin-bottom: 1rem;
-    align-items: center;
-  `;
+  //form data objetc State
+  const [data, HandleData] = useState({
+    brand: "",
+    year: "",
+    plan: "",
+  });
 
-  const Label = styled.label`
-    flex: 0 0 100px;
-  `;
+  //extracting State values from data State
+  const { brand, year, plan } = data;
 
-  const Select = styled.select`
-    display: block;
-    width: 100%;
-    padding: 1rem;
-    border: 1px solid #e1e1e1;
-    -webkit-appearance: none; //replace the default browser select style
-  `;
+  //read the data form (when user submit the form ) and put them in the data State
+  const getFormData = (e) => {
+    HandleData({
+      ...data, //copy of the data state
+      [e.target.name]: e.target.value, //put the form data inputs in the data State copy
+    });
+  };
 
-  const InputRadio = styled.input`
-    margin: 0 1rem;
-  `;
+  //error State to handle form validation error
+  const [error, handleError] = useState(false);
+
+  //handle form submit event
+  const quoteInsurance = (e) => {
+    e.preventDefault();
+
+    //form validation, only empty fields for now
+    if (brand.trim() === "" || year.trim() === "" || plan.trim() === "") {
+      handleError(true);
+      return;
+    }
+    //if previous valitadion is passed
+    handleError(false);
+
+    //calculate car's age to apply 3% of depreciation for every year, year base is 2000
+    const carAge = getYearDifference(year);
+    let yearBaseDep = 2000;
+
+    //calculate 3 %  for year on depreciation, starting for the year 2000
+    yearBaseDep -= (carAge * 3 * carAge) / 100;
+  };
 
   return (
-    <form>
+    <form onSubmit={quoteInsurance}>
+      {error ? <Error>All fields are mandatory</Error> : null}
       <FormDiv>
         <Label>Brand</Label>
-        <Select>
+        <Select name="brand" value={brand} onChange={getFormData}>
           <option value="">-- Select --</option>
           <option value="america">America</option>
           <option value="europe">Europe</option>
@@ -38,7 +108,7 @@ const InsuranceForm = () => {
       </FormDiv>
       <FormDiv>
         <Label>Year</Label>
-        <Select>
+        <Select name="year" value={year} onChange={getFormData}>
           <option value="">-- Select --</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
@@ -55,12 +125,24 @@ const InsuranceForm = () => {
 
       <FormDiv>
         <Label>Plan</Label>
-        <InputRadio type="radio" name="plan" value="basic" />
+        <InputRadio
+          type="radio"
+          name="plan"
+          value="basic"
+          checked={plan === "basic"}
+          onChange={getFormData}
+        />
         Basic
-        <InputRadio type="radio" name="plan" value="full" />
+        <InputRadio
+          type="radio"
+          name="plan"
+          value="full"
+          checked={plan === "full"}
+          onChange={getFormData}
+        />
         Full
       </FormDiv>
-      <button type="button">Quote</button>
+      <Button type="submit">Quote</Button>
     </form>
   );
 };
